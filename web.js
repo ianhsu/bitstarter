63be0e6375db708cceffa6485c03d8978a61120e
Ian Hsu <ianhsu@yahoo.com>
2013-07-12 20:11:06 +0000

    move buf variable to within function

--- web.js
+++ web.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-
 app.get('/', function(request, response) {
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
   response.send(buf.toString('utf-8'));
 });
 
